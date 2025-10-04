8983ddaafe626409b075c223ae49be80e04bf592
Sameer <sameerthecoolguy2006@gmail.com>
2025-10-05 00:24:05 +0500

    added struct GameState

--- hangman.h
+++ hangman.h
@@ -5,6 +5,20 @@
 #ifndef HANGMAN_HANGMAN_H
 #define HANGMAN_HANGMAN_H
 
+#include <stdbool.h>
+
+#define MAX_WORD_LEN 50
+#define MAX_GUESSED 26
+
+// struct representing the current state of hangman game
+typedef struct {
+    char word[MAX_WORD_LEN];        // word to guess
+    char revealed[MAX_WORD_LEN];    // underscores and revealed letters
+    char guessed[MAX_GUESSED];      // letters player guessed so far
+    int numGuessed;                 // no of letters guessed
+    int lives;                      // remaining lives
+} GameState;
+
 void test();
 
 #endif //HANGMAN_HANGMAN_H
